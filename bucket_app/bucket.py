09ba6def7e7b3894a1c0bd1e3ac2b9ac5ab9e05f
Adrian <adriand0202@gmail.com>
2024-06-15 15:51:59 +0200

    corrected

--- bucket_app/bucket.py
+++ bucket_app/bucket.py
@@ -19,9 +19,11 @@ bucket = client.bucket(bucket_name)
 
 
 def process_file(blob):
+    print("checking " + blob.name)
     if blob.name.startswith("done_") or blob.name.startswith("error_"):
         return
 
+    print("Sprawdzam rozszerzenie")
     _, extension = os.path.splitext(blob.name)
     if extension != ".json":
         new_blob = bucket.blob("error_" + blob.name)
@@ -29,23 +31,26 @@ def process_file(blob):
         blob.delete()
         print(f"Invalid file extension: {blob.name}")
         return
-
+    print("rozszerzenie ok")
     local_filename = blob.name
     blob.download_to_filename(local_filename)
 
     with open(local_filename, "r") as file:
         content = file.read()
 
+    print("sprawdzam poprawnośc json")
     try:
         data = json.loads(content)
         if "first_name" not in data or "last_name" not in data:
             raise ValueError("Invalid JSON format")
     except ValueError as e:
+        print("json nie jest poprawny")
         new_blob = bucket.blob("error_" + blob.name)
         new_blob.upload_from_filename(local_filename)
         blob.delete()
         print(f"Invalid JSON format in file: {blob.name}")
     else:
+        print("probuje wyslac requesta")
         try:
             response = requests.post(server_url + "/add_person", json=data)
             response.raise_for_status()
@@ -54,6 +59,7 @@ def process_file(blob):
             new_blob.upload_from_filename(local_filename)
             blob.delete()
         except requests.exceptions.RequestException as e:
+            print("cos poszlo nie tak z requestem")
             new_blob = bucket.blob("error_" + blob.name)
             new_blob.upload_from_filename(local_filename)
             blob.delete()
--- flask_app/run.py
+++ flask_app/run.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
-from flask_app.flask_endpoint import app
+from flask_endpoints.flask_endpoint import app
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
